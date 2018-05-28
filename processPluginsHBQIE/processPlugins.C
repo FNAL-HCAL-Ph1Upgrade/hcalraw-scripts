005852f60c41a26ebe50d8ce43ffb401faa6d9d8
Christopher_Madrid <Chris_Madrid@baylor.edu>
2018-05-27 19:50:45 -0500

    added json stuff

--- processPluginsHBQIE/processPlugins.C
+++ processPluginsHBQIE/processPlugins.C
@@ -6,6 +6,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <json/json.h>
 
 void checkFit(const FitResults* r, const PluginPassInfo& p, std::vector<bool>& flags, std::vector<TH1F*>& summaryVec)
 {
@@ -189,6 +190,19 @@ int main(int argc, char *argv[])
         }
     }
 
+    Json::Value event;   
+    Json::Value vec(Json::arrayValue);
+    vec.append(Json::Value(1));
+    vec.append(Json::Value(2));
+    vec.append(Json::Value(3));
+
+    event["competitors"]["home"]["name"] = "Liverpool";
+    event["competitors"]["away"]["code"] = 89223;
+    event["competitors"]["away"]["name"] = "Aston Villa";
+    event["competitors"]["away"]["code"]=vec;
+
+    std::cout << event << std::endl;
+    
     for(const auto& v : summaryPlots)
     {
         for(auto* s : v)
