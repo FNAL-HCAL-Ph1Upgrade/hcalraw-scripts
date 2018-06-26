cc3ebc344ac0d69d8ec304e8f5ee59974b19c33f
hcalpro <hcalpro@cmsnghcal01.fnal.gov>
2018-06-26 15:17:42 -0500

    Backing up run control output to external disk

--- processPluginsHBQIE/RunEverything.sh
+++ processPluginsHBQIE/RunEverything.sh
@@ -14,9 +14,10 @@ QCPath=/home/hcalpro/hcalraw-scripts/processPluginsHBQIE/qcTestResults
 ##################################################
 # Run the register test
 ##################################################
-echo "Running the register test"
-cd /home/hcalpro/GITrepos/Common
-python RunRegisterTest.py $runNum 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 &
+#echo "Running the register test"
+#cd /home/hcalpro/GITrepos/Common
+#python RunRegisterTest.py 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 &
+#python RunRegisterTest.py 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 &
 
 ##################################################
 # Processes the output of run control
@@ -35,27 +36,28 @@ cd /home/hcalpro/hcalraw-scripts/processPluginsHBQIE
 ##################################################
 # Moves all data to long term storage on cmshcal11
 ##################################################
-wait
-echo "Making directory on cmshcal11"
-ssh $REMOTEHOST 'cd '$UploadDir'/run_control && mkdir run'${runNum}'_output'
-
-echo "Moving files to cmshcal11"
-#Output of Frank's register test
-rsync -r /home/hcalpro/GITrepos/Common/registerTestResults/* $REMOTEHOST:$UploadDir/temp_reg_test/.
-
-#Output of run control
-rsync  /home/hcalpro/DATA/FNAL_000$runNum.root $REMOTEHOST:$UploadDir/run_control/run${runNum}_output/.
-
-#Output of Mark's plugin
-rsync /home/hcalpro/hcalraw/output/run$runNum-master.root $REMOTEHOST:$UploadDir/run_control/run${runNum}_output/.
-
-#Output of Chris' QC code
-for dir in $QCPath/QC_run$runNum/*; do
-    if [ -d "$dir" ]; then
-    	#Card info
-    	rsync -a $dir $REMOTEHOST:$UploadDir/run_control/cards/.
-    else
-    	#Run info
-    	rsync $dir $REMOTEHOST:$UploadDir/run_control/run${runNum}_output/.
-    fi
-done
+#wait
+#echo "Making directory on cmshcal11"
+#ssh $REMOTEHOST 'cd '$UploadDir'/run_control && mkdir run'${runNum}'_output'
+#
+#echo "Moving files to cmshcal11"
+##Output of Frank's register test
+#rsync -r /home/hcalpro/GITrepos/Common/registerTestResults/* $REMOTEHOST:$UploadDir/temp_reg_test/.
+#
+##Output of run control
+#rsync  /home/hcalpro/DATA/FNAL_000$runNum.root $REMOTEHOST:$UploadDir/run_control/run${runNum}_output/.
+#rsync  /home/hcalpro/DATA/FNAL_000$runNum.root /run/media/hcalpro/HCALWH14/RunControlOutput2018/.
+#
+##Output of Mark's plugin
+#rsync /home/hcalpro/hcalraw/output/run$runNum-master.root $REMOTEHOST:$UploadDir/run_control/run${runNum}_output/.
+#
+##Output of Chris' QC code
+#for dir in $QCPath/QC_run$runNum/*; do
+#    if [ -d "$dir" ]; then
+#    	#Card info
+#    	rsync -a $dir $REMOTEHOST:$UploadDir/run_control/cards/.
+#    else
+#    	#Run info
+#    	rsync $dir $REMOTEHOST:$UploadDir/run_control/run${runNum}_output/.
+#    fi
+#done
